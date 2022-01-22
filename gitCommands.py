5260de8c34710c90ea7d8056aa81c31a8a828361
Kevin Campos Venegas <kenderman.8@gmail.com>
2022-01-22 11:24:31 -0300

    Este es el primer commit de el repositorio

---
+++ gitCommands.py
@@ -0,0 +1,19 @@
+import time
+
+print("RuningSomething")
+
+
+""" COMANDOS GENERALES
+
+$git init // Inicializa el repositorio
+$git add . // Agrega todos los archivos a la lista de cambios
+$git commit -m "mensaje" // Agrega un comentario al commit y lo guarda
+
+$git status // Muestra el estado del repositorio
+$git show // Muestra el estado del repositorio
+$git log // Muestra los commits
+$git log biogragia.txt // Muestra los commits de un archivo
+$git push origin master // Envia los cambios al repositorio remoto
+
+
+"""
