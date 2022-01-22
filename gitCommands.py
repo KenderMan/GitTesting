f2a512dbac81271996a6472bf579a20b2798b6b0
Kevin Campos Venegas <kenderman.8@gmail.com>
2022-01-22 12:01:14 -0300

    agrege un txt de emojis y agrege contenido a gitCommands

--- gitCommands.py
+++ gitCommands.py
@@ -6,14 +6,19 @@ print("RuningSomething")
 """ COMANDOS GENERALES
 
 $git init // Inicializa el repositorio
-$git add . // Agrega todos los archivos a la lista de cambios
-$git commit -m "mensaje" // Agrega un comentario al commit y lo guarda
 
-$git status // Muestra el estado del repositorio
-$git show // Muestra el estado del repositorio
-$git log // Muestra los commits
-$git log biogragia.txt // Muestra los commits de un archivo
-$git push origin master // Envia los cambios al repositorio remoto
+$git add . // Agrega todos los archivos a la lista de cambios
+Este comando tambien se puede hacer en un archivo en espesifico.
+
+$git commit -m "mensaje" // Agrega un comentario al commit y lo guarda
+En este comando es importante poner el -m "mensaje" ya que sino te mandara a la consola de vim.
+Cada commit debe tener un mensaje.
+
+🔥 $git status // Muestra el estado de los archivos segun su estado de cambios
+🔥 $git show // Muestra el estado del repositorio tambien se puede consultar un commit especifico por su numero de commit
+🔥 $git log // Muestra el historial de los commits y su numero de commit
+🔥 $git log biogragia.txt // Muestra los commits de un archivo
+🔥 $git push origin master // Envia los cambios al repositorio remoto
 
 
 """
